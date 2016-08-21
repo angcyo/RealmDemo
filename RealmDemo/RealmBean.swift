8d32bab5678d3190b0e2a1ce44d968b50776ecec
angcyo <angcyo@126.com>
2016-08-21 22:25:07 +0800

    2016-08-21

---
+++ RealmDemo/RealmBean.swift
@@ -0,0 +1,19 @@
+//
+//  RealmBean.swift
+//  RealmDemo
+//
+//  Created by angcyo on 16/08/20.
+//  Copyright © 2016年 angcyo. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class RealmBean: Object {
+    
+// Specify properties to ignore (Realm won't persist these)
+    
+//  override static func ignoredProperties() -> [String] {
+//    return []
+//  }
+}
